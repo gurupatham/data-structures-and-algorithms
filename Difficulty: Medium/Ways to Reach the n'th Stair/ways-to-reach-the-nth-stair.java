2f9025a8cb557a84e875b717996903941e271438
gurupatham <45871098+gurupatham@users.noreply.github.com>
2025-03-11 23:24:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -31,28 +31,17 @@ class GFG {
 
 
 class Solution {
-    // Function to count number of ways to reach the nth stair.
-    int process(int n,int[] dp){
-        if(n<=1){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        return dp[n]=process(n-1,dp)+process(n-2,dp);
-    }
-    
-    
     int countWays(int n) {
-
         // your code here
-        if(n<=1){
-            return 1;
+        if(n<=2){
+            return n;
         }
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0]=1;
         dp[1]=1;
-        return process(n,dp);
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
