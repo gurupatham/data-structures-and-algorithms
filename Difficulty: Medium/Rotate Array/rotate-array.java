7b5972f871c1c67fc1cbfce4b3ef35efa1afd8b5
gurupatham <45871098+gurupatham@users.noreply.github.com>
2025-07-19 19:09:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate Array/rotate-array.java
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -1,45 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = Integer.parseInt(in.readLine().trim());
-        while (t-- > 0) {
-            String line = in.readLine();
-            String[] tokens = line.split(" ");
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
-            int n = array.size();
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) arr[i] = array.get(i);
-
-            new Solution().rotateArr(arr, d); // rotating the array
-            StringBuilder sb = new StringBuilder();
-
-            // printing the elements of the array
-            for (int value : arr) sb.append(value).append(" ");
-            out.println(sb.toString().trim());
-
-            out.println("~");
-        }
-        out.flush();
-        out.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
@@ -57,9 +15,10 @@ class Solution {
     
     static void rotateArr(int arr[], int d) {
         // add your code here
+        int n = arr.length-1;
         d=d%arr.length;
         reverse(arr,0,d-1);
-        reverse(arr,d,arr.length-1);
-        reverse(arr,0,arr.length-1);
+        reverse(arr,d,n);
+        reverse(arr,0,n);
     }
 }
