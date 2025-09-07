8fdb483dd50f99ea51b1c06d7bad340070aac512
gurupatham <45871098+gurupatham@users.noreply.github.com>
2025-09-07 20:22:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -1,36 +1,30 @@
-/*class Node
-{
+/*
+class Node {
     int data;
     Node next;
 
-    Node(int key)
-    {
-        data = key;
+    Node(int x) {
+        data = x;
         next = null;
     }
 }
 */
 
-// arr is an array of Nodes of the heads of linked lists
-
 class Solution {
-    // Function to merge K sorted linked list.
-    Node mergeKLists(List<Node> arr) {
-        // Add your code here.
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)-> a.data-b.data);
-        int k = arr.size();
-        for(int i=0;i<k;i++){
-            Node node = arr.get(i);
-            pq.offer(node);
-        }
+    Node mergeKLists(Node[] arr) {
+        // code here
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> a.data-b.data);
         Node ans = new Node(-1);
-        Node dummy = ans;
+        Node temp = ans;
+        for(Node each : arr){
+            pq.offer(each);
+        }
         while(!pq.isEmpty()){
-            Node node = pq.poll();
-            dummy.next=node;
-            dummy = dummy.next;
-            if(Objects.nonNull(node.next)){
-                pq.offer(node.next);
+            Node currNode = pq.poll();
+            temp.next = currNode;
+            temp = temp.next;
+            if(Objects.nonNull(currNode.next)){
+                pq.offer(currNode.next);
             }
         }
         return ans.next;
