cebc2d8727c6e91a07fd2fc0de77be44454e02eb
gurupatham <45871098+gurupatham@users.noreply.github.com>
2025-09-24 02:49:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest substring with distinct characters/longest-substring-with-distinct-characters.java
+++ Difficulty: Medium/Longest substring with distinct characters/longest-substring-with-distinct-characters.java
@@ -1,46 +1,14 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.HashMap;
-import java.util.HashSet;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine();
-            Solution ob = new Solution();
-            System.out.println(ob.longestUniqueSubstr(s));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     public int longestUniqueSubstr(String s) {
         // code here
         int ans=0;
-        HashSet<Character> set = new HashSet<>();
-        for(int l=0,r=0;r<s.length();){
-            if(set.contains(s.charAt(r))){
-                while(l<=r && set.contains(s.charAt(r))){
-                    set.remove(s.charAt(l));
-                    l++;
-                }
-                set.add(s.charAt(r++));
+        Map<Character,Integer> map = new HashMap<>();
+        for(int l=0,r=0;r<s.length();r++){
+            if(map.containsKey(s.charAt(r)) && map.get(s.charAt(r))>=l){
+                l=map.get(s.charAt(r))+1;
             }
-            else{
-                set.add(s.charAt(r));
-                ans = Math.max(ans,r-l+1);
-                r++;
-            }
+            map.merge(s.charAt(r),r,Integer::max);
+            ans = Math.max(ans,r-l+1);
         }
         return ans;
     }
